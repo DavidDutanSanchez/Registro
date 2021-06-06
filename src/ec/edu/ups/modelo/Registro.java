cd45954c8e368771378b4293ed518c46b1b180e7
surfa <surfa@SurfaceBook2>
2021-06-06 03:24:40 -0400

    primer comit

---
+++ src/ec/edu/ups/modelo/Registro.java
@@ -0,0 +1,68 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ec.edu.ups.modelo;
+
+import java.util.Date;
+
+/**
+ *
+ * @author surfa
+ */
+public class Registro {
+     private long id;
+    private String codigo;
+    private Date fechaRegistro;
+    private Persona persona;
+
+    public Registro() {
+    }
+
+    public Registro(long id, String codigo, Date fechaRegistro, Persona persona) {
+        this.id = id;
+        this.codigo = codigo;
+        this.fechaRegistro = fechaRegistro;
+        this.persona = persona;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+
+    public Date getFechaRegistro() {
+        return fechaRegistro;
+    }
+
+    public void setFechaRegistro(Date fechaRegistro) {
+        this.fechaRegistro = fechaRegistro;
+    }
+
+    public Persona getPersona() {
+        return persona;
+    }
+
+    public void setPersona(Persona persona) {
+        this.persona = persona;
+    }
+
+    @Override
+    public String toString() {
+        return "Registro{" + "id=" + id + ", codigo=" + codigo + ", fechaRegistro=" + fechaRegistro + ", persona=" + persona + '}';
+    }
+    
+    
+}
